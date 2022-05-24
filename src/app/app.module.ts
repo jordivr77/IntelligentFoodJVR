ab6838baa4845528b28aa8ad49e8294d66e67814
extjvillalonga <extjvillalonga@oesia.com>
2022-05-24 08:57:59 +0200

    Importar todos los PageModules en app.module,ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,13 @@ import { HttpClientModule, HttpClient} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AgregarAlimentoPageModule } from './agregar-alimento/agregar-alimento.module';
 import { AlimentosPageModule } from './alimentos/alimentos.module';
+import { CalendarioPageModule } from './calendario/calendario.module';
+import { HomePageModule } from './home/home.module';
+import { HoyPageModule } from './hoy/hoy.module';
+import { TusRecetasPageModule } from './tus-recetas/tus-recetas.module';
+
 import { AlimentoService } from './servicios/alimento.service';
 import { CategoriaService } from './servicios/categoria.service';
 import { AgregarAlimentoService } from './servicios/agregar-alimento.service';
@@ -23,7 +29,13 @@ import { AgregarAlimentoService } from './servicios/agregar-alimento.service';
     IonicModule.forRoot(), 
     AppRoutingModule, 
     HttpClientModule, 
-    AlimentosPageModule
+    AlimentosPageModule,
+    CalendarioPageModule,
+    AgregarAlimentoPageModule,
+    HomePageModule,
+    HoyPageModule,
+    TusRecetasPageModule
+
   ], 
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
